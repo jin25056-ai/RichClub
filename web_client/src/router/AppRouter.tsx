5dcecda7bab8729862241c7e27505a7e131008d9
efforthye <efforthye@gmail.com>
2026-06-24 18:33:02 +0900

    refactor: move ExamplePage to /, remove MainPage/ChartPage routes

--- web_client/src/router/AppRouter.tsx
+++ web_client/src/router/AppRouter.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import MainPage from '../pages/MainPage';
 import AuthPage from '../pages/AuthPage';
-import ChartPage from '../pages/ChartPage';
 import ExamplePage from '../pages/ExamplePage';
 import MLOpsDashboard from '../pages/MLOpsDashboard';
 
@@ -10,10 +8,8 @@ const AppRouter: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<ExamplePage />} />
         <Route path="/auth" element={<AuthPage />} />
-        <Route path="/chart" element={<ChartPage />} />
-        <Route path="/example" element={<ExamplePage />} />
         <Route path="/mlops" element={<MLOpsDashboard />} />
       </Routes>
     </BrowserRouter>
